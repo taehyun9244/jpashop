63304a055a88dfb39cf323146edd31391fea5c3e
taehyun9244 <85472257+taehyun9244@users.noreply.github.com>
2022-05-09 17:13:42 +0900

    fetch join

--- src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -6,6 +6,8 @@ import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import jpabook.jpashop.repository.order.simplequery.OrderSimpleQueryDto;
+import jpabook.jpashop.repository.order.simplequery.OrderSimpleQueryRepository;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,6 +22,7 @@ import java.util.stream.Collectors;
 public class OrderSimpleApiController {
 
     private final OrderRepository orderRepository;
+    private final OrderSimpleQueryRepository queryRepository;
 
     @GetMapping("api/v1/simple-orders")
     public List<Order> ordersV1(){
@@ -33,12 +36,34 @@ public class OrderSimpleApiController {
 
     @GetMapping("api/v2/simple-orders")
     public List<SimpleOrderDto> ordersV2(){
-        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
-        List<SimpleOrderDto> result = orders.stream()
+
+        //ORDER 2개
+        //1 + N의 문제 -> 1 + 회원 + 배송송
+       List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+
+        //주문 결과가 2개 -> 2번 루프 돈것
+       List<SimpleOrderDto> result = orders.stream()
                 .map(o -> new SimpleOrderDto(o))
                 .collect(Collectors.toList());
         return result;
     }
+
+    @GetMapping("api/v3/simple-orders")
+    public List<SimpleOrderDto> ordersV3() {
+        List<Order> orders = orderRepository.findAllWithMemberDelivery();
+        List<SimpleOrderDto> result = orders.stream()
+                .map(o -> new SimpleOrderDto(o))
+                .collect(Collectors.toList());
+
+        return result;
+    }
+
+    @GetMapping("api/v4/simple-orders")
+    public List<OrderSimpleQueryDto> ordersV4() {
+       return queryRepository.findOrderDtos();
+    }
+
+
 
     @Data
     static class SimpleOrderDto{
---
+++ src/main/java/jpabook/jpashop/api/dto/OrderDto.java
@@ -0,0 +1,50 @@
+package jpabook.jpashop.api.dto;
+
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.domain.OrderStatus;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Getter
+public class OrderDto {
+
+    private Long orderId;
+    private String name;
+    private LocalDateTime orderDate;
+    private OrderStatus orderStatus;
+    private Address address;
+    private List<OrderItemDto> orderItems;
+
+    public OrderDto(Order order) {
+        orderId = order.getId();
+        name = order.getMember().getName();
+        orderDate = order.getOrderDate();
+        orderStatus = order.getStatus();
+        address = order.getDelivery().getAddress();
+        order.getOrderItems().stream().forEach(o -> o.getItem().getName());
+        orderItems = order.getOrderItems().stream()
+                .map(orderItems -> new OrderItemDto(orderItems))
+                .collect(Collectors.toList());
+    }
+
+    @Getter
+    static class OrderItemDto{
+        private String itemName;
+        private int orderPrice;
+        private int count;
+
+        public OrderItemDto(OrderItem orderItem){
+            itemName = orderItem.getItem().getName();
+            orderPrice = orderItem.getOrderPrice();
+            count = orderItem.getCount();
+        }
+
+    }
+}
+
+
